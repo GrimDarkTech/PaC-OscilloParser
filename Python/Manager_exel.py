e51aaed7b8e4f654ecc95a973670f2847c411574
kms0207 <75982005+kms0207@users.noreply.github.com>
2024-02-09 12:24:28 +0400

    #Excel manager hotfix

--- Python/Manager_exel.py
+++ Python/Manager_exel.py
@@ -14,24 +14,23 @@ class ExcelManager:
         return book
 
     @staticmethod
-    def write_to_excel(file_path: str, values, column_num = 1):
+    def write_to_excel(bookName: str, title: str , values, column_num = 1):
         # column_num - Номер столбца, в который будем записывать значения
         # values - Массив значений
         # file_path - Имя файла
 
         # Открываем файл Excel
-        wb = openpyxl.load_workbook(file_path)
+        wb = ExcelManager.OpenBook(bookName)
 
         # Выбираем активный лист
         sheet = wb.active
-
+        #Write title
+        sheet.cell(row=1, column=column_num).value = title
         # Проходим по значениям и записываем их во второй столбец
-        for row_num, value in enumerate(values, start=1):
+        for row_num, value in enumerate(values, start=2):
             # Записываем значение в указанную ячейку
             sheet.cell(row=row_num, column=column_num).value = value
 
         # Сохраняем изменения в файл
-        wb.save(file_path)
+        wb.save(f"Excels/{bookName}.xlsx")
         wb.close()
-
-ExcelManager.write_to_excel('conn.xlsx', (1, 2, 3, 4, 5), 7)
