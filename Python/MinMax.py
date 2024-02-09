7597308f639313dd1fca65f6801dcd51dbd90611
kms0207 <75982005+kms0207@users.noreply.github.com>
2024-02-09 12:24:00 +0400

    #DataAnalyser truncat method added; getExtremums values method

--- Python/MinMax.py
+++ Python/MinMax.py
@@ -9,8 +9,8 @@ class DataAnalyzer:
     @staticmethod
     def extremumsIndexSearch(values: list) -> tuple:
         """Returns tuple of list of local minima indexes and a list of local maxima indexes of float array"""
-        maxesIndexes = [0]
-        minsIndexes = [0]
+        maxesIndexes = []
+        minsIndexes = []
 
         #Checking first value of array
         if(values[0] >= values[1]):
@@ -30,14 +30,38 @@ class DataAnalyzer:
                 maxesIndexes.append(i)
             elif (values[i] <= values[i+1] and values[i] <= values[i-1]):
                 minsIndexes.append(i)
-
         return (minsIndexes, maxesIndexes)
+    
+    @staticmethod
+    def getExtremumsValues(values: list, minsIndexes: list, maxsIndexes: list) -> tuple:
+        """Returns tuple of list of minima values and list of maxima values"""
+        minsValues = []
+        for i in range(0, len(minsIndexes)):
+            minsValues.append(values[i])
+            maxsValues = []
+        for i in range(0, len(maxsIndexes)):
+            maxsValues.append(values[i])
+        return(minsValues, maxsValues)
+
+    @staticmethod
+    def getExtremumsTimes(minsIndexes: list, maxsIndexes: list, timeStep: float, startTime: float) -> tuple:
+        """Returns tuple of list of minima times and list of maxima times"""
+        minsTimes = []
+        for i in range(0, len(minsIndexes)):
+            minsTimes.append(startTime + minsIndexes[i] * timeStep)
+        maxsTimes = []
+        for i in range(0, len(maxsIndexes)):
+            maxsTimes.append(startTime + maxsIndexes[i] * timeStep)
+        return(minsTimes, maxsTimes)
 
     @staticmethod
     def drawPlot(dataValues: list, timeStep: float, drawMin: bool, drawMax: bool, **kargs):
         """Draws plot of array of function values with fixed time step"""
         minsIndexes = kargs.get('minsIndexes', [])
         maxesIndexes = kargs.get('maxesIndexes', [])
+        showPlot = kargs.get('showPlot', [])
+        savePlot = kargs.get('savePlot', [])
+        fileName = kargs.get('fileName', [])
         #Calculate time 
         dataTime = []
         for i in range(0, len(dataValues)):
@@ -50,5 +74,26 @@ class DataAnalyzer:
         if(drawMax):
             plot.plot(dataTime, dataValues, '-b', markevery=maxesIndexes, marker=8, markerfacecolor='red', markeredgecolor='red')
         #Drawing plot
-        plot.show()
+        if(savePlot):
+            plot.savefig(f"Plots/{fileName}.pdf")
+            print(f"Plot image has been successfully uploaded to Plots/{fileName}.pdf") 
+        if(showPlot):
+            plot.show()
+        
+    @staticmethod
+    def truncatData(values:list,mathematical_expectation:float,square_deviation:float, timeStep: float) -> tuple:
+        """Returns truncated list of values that go beyond the mathematical expectation of the +-square deviation"""
+        indexs = []
+        for i in range (0, len(values)):
+            if values[i] > mathematical_expectation + square_deviation or values[i] < mathematical_expectation - square_deviation:
+                indexs.append(i)
+        for i in range (0, len(indexs)):
+            if indexs[1]-indexs[0] > 5:
+                indexs.pop(0)
+        cutValues = []
+        for i in range (indexs[0], indexs[len(indexs)-1]):
+            cutValues.append(values[i])
+        startTime = indexs[0] * timeStep
+        endTime = indexs[len(indexs)-1] * timeStep
+        return (cutValues, startTime, endTime)
 
